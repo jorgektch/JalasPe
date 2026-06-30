68bd30fe492a0826a30c1e5eade7d59a8d953170
Jorge Jesús <jorgektch@gmail.com>
2026-06-29 19:32:22 -0500

    update core

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,7 +9,5 @@ export const environment = {
     messagingSenderId: "229008340318",
     appId: "1:229008340318:web:c73702b3cbff5c39ec8bcf",
     measurementId: "G-8Q70PRHCSF"
-  },
-  // Apuntamos temporalmente al puerto 8000 (FastAPI) mientras desarrollamos en local
-  apiUrl: "http://localhost:8000" 
+  }
 };
